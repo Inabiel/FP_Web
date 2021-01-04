c9f5d0d8b8fe02ad170171457bd3d272fccbc925
Nabiel Izzullah Pansuri <nabiel.p@students.amikom.ac.id>
2021-01-04 11:55:16 +0700

    adding boilerplate at home

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -1,5 +1,17 @@
-<?php $this->load->view("user/header") ?>
-<div style="height: 500px;">
-  Hii
-</div>
-<?php $this->load->view("user/footer") ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php $this->load->view("user/header") ?>
+    <div class="container" style="min-height:500px">
+    </div>
+    <?php $this->load->view("user/footer") ?>
+</body>
+
+</html>
