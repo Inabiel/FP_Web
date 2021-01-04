e06b91af37ee050957be4a1426276325e3ac87f0
nickoaji <nicko.10@students.amikom.ac.id>
2021-01-04 09:34:03 +0700

    membuat template

---
+++ application/views/user/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Bengkel Las Laras</title>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
+  <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery/jquery.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+  <nav class="navbar fixed-top navbar-expand-lg navbar-laras">
+    <div class="container">
+      <a class="navbar-brand" href="<?php echo base_url(); ?>#">
+        <img src="<?php echo base_url(); ?>assets/images/logo.svg" alt="" width="30" height="24">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" aria-current="page" href="<?php echo base_url(); ?>#">Home</a>
+          <a class="nav-link" href="<?php echo base_url(); ?>#">Profil</a>
+          <a class="nav-link" href="<?php echo base_url(); ?>#">Jasa</a>
+          <a class="nav-link" href="<?php echo base_url(); ?>#">Galeri</a>
+          <a class="nav-link" href="<?php echo base_url(); ?>#">Register</a>
+          <a class="nav-link" href="<?php echo base_url(); ?>#">Login</a>
+          <a class="nav-link" href="<?php echo base_url(); ?>#">Shopping Cart</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+</body>
+
+</html>
