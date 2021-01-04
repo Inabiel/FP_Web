c7143b1d5046dd696a353d2d02c209ac36732d6e
Nabiel Izzullah Pansuri <nabiel.p@students.amikom.ac.id>
2021-01-04 19:21:57 +0700

    adding galery page; adding images; changing routes

--- application/views/user/galeri.php
+++ application/views/user/galeri.php
@@ -8,8 +8,36 @@
 </head>
 
 <body>
-    <?php $this->load->view("user/header") ?>
     <div class="container" style="min-height:500px">
+        <?php $this->load->view("user/header") ?>
+        <h3 class="text-center mt-3">GALERI</h3>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-12">
+                    <img class="img-thumbnail" src="<?php echo base_url('assets/images/1.jpg')?>" alt="" srcset="">
+                </div>
+                <div class="col-md-4 col-12">
+                    <img class="img-thumbnail" src="<?php echo base_url('assets/images/welding.jpg')?>" alt=""
+                        srcset="">
+                </div>
+                <div class="col-md-4 col-12">
+                    <img class="img-thumbnail" src="<?php echo base_url('assets/images/1.jpg')?>" alt="" srcset="">
+                </div>
+            </div>
+            <div class="row pb-5">
+                <div class="col-md-4 col-12 pt-4">
+                    <img class="img-thumbnail" src="<?php echo base_url('assets/images/welding.jpg')?>" alt=""
+                        srcset="">
+                </div>
+                <div class="col-md-4 col-12 pt-4">
+                    <img class="img-thumbnail" src="<?php echo base_url('assets/images/1.jpg')?>" alt="" srcset="">
+                </div>
+                <div class="col-md-4 col-12 pt-4">
+                    <img class="img-thumbnail" src="<?php echo base_url('assets/images/welding.jpg')?>" alt=""
+                        srcset="">
+                </div>
+            </div>
+        </div>
     </div>
     <?php $this->load->view("user/footer") ?>
 </body>
