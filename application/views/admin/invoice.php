fb525a22eb237f3cfa3918e2cbc6b0ccc0deb0ab
nickoaji <nicko.10@students.amikom.ac.id>
2021-01-12 18:57:09 +0700

    tambah templte view invoice

--- application/views/admin/invoice.php
+++ application/views/admin/invoice.php
@@ -2,46 +2,61 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Bengkel Las Laras</title>
+  <?php $this->load->view("admin/_parts/head") ?>
 </head>
 
 <body>
   <?php
-  if ($this->session->userdata('role_id') == '') {
-    redirect('auth/login');
-  } ?>
+  if ($this->session->userdata('role_id') != 'Admin') {
+    redirect('not_admin');
+  }
+  ?>
+  <div id="wrapper">
 
-  <?php $this->load->view("user/header") ?>
-  <div class="container">
-    <h4 class="text-center mt-4 mb-4">Shopping Cart Anda</h4>
-    <table class="table table-bordered table-hover table-striped">
-      <tr>
-        <th>Id Invoice</th>
-        <th>Nomor Telepon</th>
-        <th>Alamat</th>
-        <th>Keterangan</th>
-        <th>Tanggal Pesan</th>
-        <th>Batas Bayar</th>
-        <th>Aksi</th>
-      </tr>
-      <?php foreach ($invoice as $inv) : ?>
-        <tr>
-          <td><?php echo $inv->id ?></td>
-          <td><?php echo $inv->no_telp ?></td>
-          <td><?php echo $inv->alamat ?></td>
-          <td><?php echo $inv->keterangan ?></td>
-          <td><?php echo $inv->tgl_pesan ?></td>
-          <td><?php echo $inv->batas_bayar ?></td>
-          <td>
-            <div class="btn btn-sm btn-primary">Detail</div>
-          </td>
-        </tr>
-      <?php endforeach; ?>
-    </table>
+    <?php $this->load->view("admin/_parts/sidebar") ?>
+
+    <div id="content-wrapper">
+      <?php $this->load->view("admin/_parts/topbar") ?>
+      <div class="container">
+        <h4 class="text-center mt-4 mb-4">Daftar Penjualan</h4>
+        <table class="table table-bordered table-hover table-striped">
+          <tr>
+            <th>Id Invoice</th>
+            <th>Nomor Telepon</th>
+            <th>Alamat</th>
+            <th>Keterangan</th>
+            <th>Tanggal Pesan</th>
+            <th>Batas Bayar</th>
+            <th>Aksi</th>
+          </tr>
+          <?php foreach ($invoice as $inv) : ?>
+            <tr>
+              <td><?php echo $inv->id ?></td>
+              <td><?php echo $inv->no_telp ?></td>
+              <td><?php echo $inv->alamat ?></td>
+              <td><?php echo $inv->keterangan ?></td>
+              <td><?php echo $inv->tgl_pesan ?></td>
+              <td><?php echo $inv->batas_bayar ?></td>
+              <td>
+                <div class="btn btn-sm btn-primary">Detail</div>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        </table>
+      </div>
+      <?php $this->load->view("admin/_parts/footer") ?>
+
+    </div>
+    <!-- /.content-wrapper -->
+
   </div>
-  <?php $this->load->view("user/footer") ?>
+  <!-- /#wrapper -->
+
+
+  <?php $this->load->view("admin/_parts/scrolltop") ?>
+  <?php $this->load->view("admin/_parts/modal") ?>
+  <?php $this->load->view("admin/_parts/js") ?>
+
 </body>
 
 </html>
